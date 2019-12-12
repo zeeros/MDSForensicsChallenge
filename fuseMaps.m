cb1e305666ce92efc2a9844f95bd46dd6b209c10
Marian Diaconu <neboduus@gmail.com>
2019-12-12 18:29:10 +0100

    adds definition of fuseMaps() - needs to be implemented

---
+++ fuseMaps.m
@@ -0,0 +1,9 @@
+function M = fuseMaps(maps)
+    % it takes an array of maps and 
+    % fuse them into a single map
+    [dimx, dimy] = size(maps);
+    numberOfMaps = max(dimx, dimy);
+    for i=1:numberOfMaps
+    end
+end
+
