631d1e8c7dae244331924a861a12e44ed139805c
Marian Diaconu <neboduus@gmail.com>
2019-12-13 13:44:00 +0100

    modifies evaluate_datasets() in order to evaluate only images present in DEMO-RESULT

--- Evaluation/evaluate_datasets.m
+++ Evaluation/evaluate_datasets.m
@@ -1,15 +1,20 @@
 function measure = evaluate_datasets(ground_truth_path, estimation_path)
     measures = [];
-    files = dir(strcat(estimation_path,filesep,"*.bmp"));
-    for k = 1:length(files)
-        f_name = files(k).name;
-        e_mask = strcat(estimation_path,filesep,f_name);
-        gt_mask = strcat(ground_truth_path,filesep,f_name);
-        map_gt=double(imread(gt_mask));
-        map_est = double(imread(e_mask));
-        measure = f_measure(map_gt,map_est);
+    estimatedFiles = findFiles(estimation_path, 'bmp');
+    
+    for k = 1:length(estimatedFiles)
+        eMPath = char(estimatedFiles(k));
+        parts = strsplit(eMPath, '/');
+        partsSizes = size(parts);
+        fname = parts(partsSizes(2));
+        gMPath = strcat(ground_truth_path, fname{1});
+        eM = imread(eMPath);
+        gM = imread(gMPath);
+        eM = double(eM);
+        gM = double(gM);
+        measure = f_measure(gM,eM);
         measures = [measures measure];
-        fprintf('%s -> %3.4f\n',f_name,measure);
+        %fprintf('%s -> %3.4f\n',f_name,measure);
     end
     measure = mean(measures);
 end
