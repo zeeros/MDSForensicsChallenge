5632b18c525d5722fd62c74450c55b9e9505e360
Marian Diaconu <neboduus@gmail.com>
2019-12-14 12:31:18 +0100

    first results of fusion(blq,adq)

--- get_map.m
+++ get_map.m
@@ -1,5 +1,7 @@
-function get_map(image_path)
+function measure = get_map(image_path)
     outputDir = './DEMO-RESULTS/test/';
+    trueMapsDir = '../MDSDataset/dev-dataset-maps/';
+    
     % reading the image 
     image_path = char(image_path);
     I_matrix = imread(image_path);
@@ -13,6 +15,8 @@ function get_map(image_path)
     ext = parts(2);
     Iname = parts(1);
     Iname = Iname{1};
+    trueMapFName = strcat(trueMapsDir, Iname, '.bmp');
+    trueMap = imread(trueMapFName);
     if (strcmp(ext,'jpg'))
         I_struct = jpeg_read(image_path);
     end
@@ -24,41 +28,49 @@ function get_map(image_path)
     %BLK
     square = imread("Algorithms/BLK/square.bmp");
     
-    %map_NOI5 = NOI5(I_matrix);
-    %map_BLK = BLK(I_matrix, square);
-    %tampering_map = 0;
+    map_NOI5 = NOI5(I_matrix);
+    map_BLK = BLK(I_matrix, square);
+    tampering_map = map_BLK;
     if (isstruct(I_struct))
         map_ADQ2 = ADQ2(I_struct);
-        %tampering_map = fuseMaps(map_BLK, map_ADQ2, 1);
-        imwrite(map_ADQ2, strcat(outputDir, Iname, '.bmp'));
+        tampering_map = fuseMaps(tampering_map, map_ADQ2, 0);
+        measure = f_measure(trueMap,tampering_map);
+        fprintf('%s -> %3.4f\n',Iname,measure);
+        
+%         if measure < 0.4
+%             subplot(231);
+%             imshow(I_matrix);
+%             title('Forged Image');
+% 
+%             subplot(232);
+%             imshow(trueMap);
+%             title('Expected Map');
+% 
+%             subplot(233);
+%             imshow(tampering_map);
+%             title('Output Map');
+% 
+%             subplot(234);
+%             imshow(map_ADQ2);
+%             title('ADQ2');
+% 
+%             subplot(235);
+%             imshow(map_BLK);
+%             title('BLK');
+% 
+%             subplot(236);
+%             imshow(map_NOI5);
+%             title('NOI5');
+%             
+%             pause;
+%         end
     end
-    %tampering_map = fuseMaps(tampering_map, map_NOI5, 0);
-    %write output in DEMO-RESULTS as a bitmap
-    %imwrite(tampering_map, strcat(outputDir, Iname, '.bmp'));
     
-%     subplot(231);
-%     imshow(I_matrix);
-%     title('Forged Image');
-%     
-%     subplot(232);
-%     imshow(M);
-%     title('Expected Map');
-%     
-%     subplot(233);
-%     imshow(uint8(tampering_map));
-%     title('Output Map Map');
-%     
-%     subplot(234);
-%     imshow(map_ADQ2);
-%     title('ADQ2');
-%     
-%     subplot(235);
-%     imshow(map_BLK);
-%     title('BLK');
-%     
-%     subplot(236);
-%     imshow(map_NOI5);
-%     title('NOI5');
+    %write output in DEMO-RESULTS as a bitmap
+    imwrite(uint8(tampering_map), strcat(outputDir, Iname, '.bmp'));
+    
+    
+
 end
 
 
--- get_maps.m
+++ get_maps.m
@@ -2,10 +2,14 @@ function get_maps(images_path)
     tifFiles = findFiles(images_path, 'tif');
     jpgFiles = findFiles(images_path, 'jpg');
     files = cat(2, jpgFiles, tifFiles);
-    [r, filesCount] = size(files);
-
-    for i=1:5
-        get_map(files(i));
+    [~, filesCount] = size(files);
+    measures = zeros(1,length(filesCount), 'int8');
+    
+    for i=1:filesCount
+        measures(i) = get_map(files(i));
     end
+    
+    m = mean(measures);
+    fprintf('mean(scores) -> %3.4f\n',m);
     
 end
