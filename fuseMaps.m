e74a0417d14b3292fcfe35fb25b62b4d77c8e046
ameeeeeeeeer <57093492+ameeeeeeeeer@users.noreply.github.com>
2019-12-15 00:43:13 +0100

    update fuseMaps.m

--- fuseMaps.m
+++ fuseMaps.m
@@ -11,6 +11,30 @@ function m1 = fuseMaps(m1, m2, method)
     end
     m1 = uint8(m1);
     m2 = uint8(m2);
+    
+    nhood = ones(100,100);
+    
+    % Fills holes smaller than 100x100
+    m1 = imclose(m1,nhood);
+    m2 = imclose(m2,nhood);
+    % Eliminates pieces smaller than 100x100
+    m1 = imopen(m1,nhood);
+    m2 = imopen(m2,nhood);
+    
+    %imagesc(m1);
+    
+    %discard highly improbable maps, i.e. sparse
+    
+    coeff1 = nnz(m1)/numel(m1);
+    coeff2 = nnz(m2)/numel(m2);
+    if (coeff1 < 0.05  ||  coeff1 > 0.8) && (coeff2 > 0.05  &&  coeff2 < 0.8)
+        % m1 just stays the same
+    elseif (coeff2 < 0.05  ||  coeff2 > 0.8) && (coeff1 > 0.05  &&  coeff1 < 0.8)
+        m1 = m2;
+    end
+    
+    %now, they're either both plausible or none of them.
+        
     if method==0
         % simple OR
         m1 = or(m1, m2);
